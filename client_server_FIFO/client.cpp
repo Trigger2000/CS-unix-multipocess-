04ea99eeae89e84416f82b5060f6c064c04c2b92
Trigger2000 <44003024+Trigger2000@users.noreply.github.com>
2019-11-29 23:15:44 +0300

    Update client.cpp

--- client_server_FIFO/client.cpp
+++ client_server_FIFO/client.cpp
@@ -10,14 +10,14 @@
 
 int main(int argc, char** argv)
 {    
-    !1! pid_t pid = getpid();
+    pid_t pid = getpid();
     char* local_fifo = (char*)calloc(6, sizeof(char));
     sprintf(local_fifo, "%d", pid);
     mkfifo(local_fifo, 0777);
 
     int fd = open("mypipe.p", O_WRONLY | O_NONBLOCK);
     write(fd, local_fifo, 5);
-    !1!
+    
     
     int reply = open(local_fifo, O_RDONLY | O_NONBLOCK);
     sleep(1);
