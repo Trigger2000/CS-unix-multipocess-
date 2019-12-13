ae6c49f7b38ade9f5e3bcb5a322520ffdcb99ab5
Ilya <trubachev.ii@phystech.edu>
2019-12-13 15:32:31 +0300

    release

--- signals/bitdelivery.cpp
+++ signals/bitdelivery.cpp
@@ -11,39 +11,29 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <sys/prctl.h>
 
 int globalbit = 0;
-int check = 0;
-
-void sendmsg (char buf, sigset_t* set);
-void rcvmsg (int chldpid, sigset_t* set);
 
 void setzero (int signum)
 {
-    printf("z\n");
     globalbit = 0;
 }
 
 void setone (int signum)
 {
-    //printf("o\n");
     globalbit = 1;
 }
 
-void setcheck (int signum)
+void finish (int signum)
 {
-    //printf("c\n");
-    check = 1;
+    exit(EXIT_SUCCESS);
 }
 
-void handle (int signum)
-{
-    printf("h\n");
-}
+void handle (int signum) {};
 
 int main(int argc, char** argv)
 {
-
     int file = open(argv[1], O_RDONLY);
     if (file == -1)
     {
@@ -51,94 +41,85 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    sigset_t set;
-    sigfillset (&set);
-    sigdelset (&set, SIGCHLD);
-    sigdelset (&set, SIGUSR1);
-    sigdelset (&set, SIGUSR2);
-    sigdelset (&set, SIGHUP);
-    sigdelset (&set, SIGINT);
-    sigprocmask(SIG_BLOCK, &set, NULL);
+    sigset_t set, old_set;
+    sigemptyset(&set);
+    sigemptyset(&old_set);
+    sigaddset(&set, SIGUSR1);
+    sigaddset(&set, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &set, &old_set);
 
     struct sigaction actzero, actone, actchld, actmsg;
     actone.sa_handler = setone;
     actzero.sa_handler = setzero;
-    actchld.sa_handler = setcheck;
+    actchld.sa_handler = finish;
     actmsg.sa_handler = handle;
+    actone.sa_flags = SA_RESTART;
+    actzero.sa_flags = SA_RESTART;
+    actchld.sa_flags = SA_RESTART;
+    actmsg.sa_flags = SA_RESTART;
     
     sigaction(SIGUSR1, &actzero, NULL);
     sigaction(SIGUSR2, &actone, NULL);
     sigaction(SIGCHLD, &actchld, NULL);
     sigaction(SIGHUP, &actmsg, NULL);
 
+    pid_t parent_id = getpid();
     pid_t pid = fork();
+
     if (pid == 0)
     {
+        prctl(PR_SET_PDEATHSIG, SIGCHLD);
+
+        if (getppid() != parent_id)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        sigset_t setch, old_setch;
+        sigemptyset(&setch);
+        sigaddset(&setch, SIGHUP);
+        sigprocmask(SIG_BLOCK, &setch, &old_setch);
+
         char buf = 0;
         while (read(file, &buf, 1) != 0)
         {
-            //printf("child\n");
-            sendmsg(buf, &set);
+            for (int i = 0; i < 8; ++i)
+            {
+                int bit = buf & 128;
+                bit = bit >> 7;
+                buf = buf << 1;
+                //sleep(1);
+                if (bit == 0)
+                {
+                    kill(getppid(), SIGUSR1);
+                }
+                else
+                {
+                    kill(getppid(), SIGUSR2);
+                }
+
+                sigsuspend(&old_set);
+            }
+
             buf = 0;
         }
-
-        //kill(getppid(), SIGCHLD);
     }
     else
     {
-        rcvmsg(pid, &set);
-        wait(0);
+        while(1)
+        {
+            char buf = 0;
+            for (int i = 0; i < 8; ++i)
+            {
+                sigsuspend(&old_set);
+                buf = buf << 1;
+                buf = buf | globalbit;
+                kill(pid, SIGHUP);
+            }
+
+            write(STDOUT_FILENO, &buf, 1);
+        }
     }
-    
 
     exit(EXIT_SUCCESS);
-}
-
-void sendmsg (char buf, sigset_t* set)
-{
-    for (int i = 0; i < 8; ++i)
-    {
-        int bit = buf & 128;
-        bit = bit >> 7;
-        buf = buf << 1;
-        
-        //sigsuspend(set);
-        if (bit == 0)
-        {
-            kill(getppid(), SIGUSR1);
-        }
-        else
-        {
-            kill(getppid(), SIGUSR2);
-        }
-        //printf("bit\n");
-        sigsuspend(set);
-        //printf("scdcf");
-    }
-}
-
-void rcvmsg (int chldpid, sigset_t* set)
-{
-    while(1)
-    {
-        char buf = 0;
-        for (int i = 0; i < 8; ++i)
-        {
-            sigsuspend(set);
-            //printf("entr\n");
-            buf = buf << 1;
-            buf = buf | globalbit;
-            kill(chldpid, SIGHUP);
-
-            if (check == 1)
-                break;
-        }
-
-        //printf("sass\n");
-        //printf("%c\n", buf);
-        write(1, &buf, 1);
-
-        if (check == 1)
-            break;
-    }
 }
